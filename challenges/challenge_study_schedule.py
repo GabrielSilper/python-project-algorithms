09cf618dc9ca6e703bff6db16b8f1d3dd483c2db
GabrielSilper <gabrielsilper.main@gmail.com>
2023-07-09 12:46:05 -0400

    R01 - Algoritmo feito.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,22 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def study_schedule(permanence_period: list, target_time: int = -1) -> int:
+    if verify_permanence_is_invalid(permanence_period):
+        return None
+
+    if type(target_time) is int and target_time > -1:
+        count = 0
+        for arrived, departure in permanence_period:
+            if arrived <= target_time <= departure:
+                count += 1
+        return count
+    return None
+
+
+def verify_permanence_is_invalid(permanence_period: list) -> bool:
+    for arrived, departure in permanence_period:
+        if arrived is None or departure is None:
+            return True
+        if type(arrived) is not int:
+            return True
+        if type(departure) is not int:
+            return True
+    return False
