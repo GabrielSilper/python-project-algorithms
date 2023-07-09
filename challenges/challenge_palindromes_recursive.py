3398dd3527de0ed3328fcb8b9114f9d7e51f4c09
GabrielSilper <gabrielsilper.main@gmail.com>
2023-07-09 18:00:32 -0400

    R03 - Algoritmo de palíndromos de forma recursiva feito.

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,19 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(word, str) or word == "":
+        return False
+
+    if low_index == high_index:
+        return True
+
+    if low_index + 1 == high_index:
+        return word[low_index] == word[high_index]
+
+    if word[low_index] == word[high_index]:
+        return True and is_palindrome_recursive(
+            word, low_index + 1, high_index - 1
+        )
+
+
+if __name__ == "__main__":
+    word = "OiiO"
+    print(is_palindrome_recursive(word, 0, len(word) - 1))
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,25 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    # testes para tipos inválidos e verifica se retornar uma exceção
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(3, 3)
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("test", "error")
+
+    # teste para casos de uma key ímpar
+    initial_message = "123456"
+    assert_message = "321_654"
+    assert encrypt_message(initial_message, 3) == assert_message
+
+    # teste para casos de uma key par
+    initial_message = "123456"
+    assert_message = "6543_21"
+    assert encrypt_message(initial_message, 2) == assert_message
+
+    # teste para casos de uma key válida por sem índice na messagem
+    initial_message = "123456"
+    assert_message = "654321"
+    assert encrypt_message(initial_message, 8) == assert_message
