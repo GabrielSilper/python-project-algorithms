9fff6f1503105ad848eba5b401e574dfa3807ec6
GabrielSilper <gabrielsilper.main@gmail.com>
2023-07-11 11:03:49 -0400

    R06 - Algoritmo de palíndromos de forma interativa feito.

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,14 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(word, str) or word == "":
+        return False
+
+    mid = len(word) // 2
+
+    for index in range(mid):
+        if word[index] != word[len(word) - 1 - index]:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    print(is_palindrome_iterative("oiio"))
